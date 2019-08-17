d30410283910065c60411380527ef902271ad6c4
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-08-17 00:05:37 -0500

    i scrapped the original idea for something new. Will be adding more to this later

--- materialize/js/myJS.js
+++ materialize/js/myJS.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
+
+/*
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+
+    $('#aboutMeAndPics').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $("#aboutMePic").addClass('slideInLeft');
+        $("#aboutmeInfo").addClass('slideInRight');
+      } 
+    });
   });
+});
+*/
