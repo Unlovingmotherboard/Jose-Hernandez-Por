9568db5dd9a71c4e5c62432d6d6a01d2938435a3
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-09-11 21:59:23 -0500

    adding amazon-in-node to my portfolio.

--- materialize/js/myJS.js
+++ materialize/js/myJS.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  ScrollReveal().reveal('.projectTitle', { delay: 500 });
+  ScrollReveal().reveal('#gitHubIcon', { delay: 800 });
+  ScrollReveal().reveal('.projectDesc', { delay: 1200 });
+  ScrollReveal().reveal('.imageDiv', { delay: 1500 });
+
+  ScrollReveal().reveal('#aboutMeNameTitle', { delay: 500 });
+  ScrollReveal().reveal('#aboutMeNameTitle', { delay: 800 });
+  ScrollReveal().reveal('#aboutMeParagraph', { delay: 1000 });
+
+  ScrollReveal().reveal('#linkdenIcon', { delay: 1000 });
+  ScrollReveal().reveal('#githubIcon', { delay: 1200 });
+  ScrollReveal().reveal('#emailIcon', { delay: 1300 });
+  ScrollReveal().reveal('#resumeIcon', { delay: 1400 });
+
+
   var firebaseConfig = {
     apiKey: "AIzaSyCp4SRVYUy1bIhVReZFiQtPPMyuY-aejb0",
     authDomain: "myportfolio-analytics.firebaseapp.com",
@@ -14,7 +29,7 @@ $(document).ready(function () {
 
 
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
 
   gtag('config', 'UA-64399820-1');
@@ -55,10 +70,13 @@ $(document).ready(function () {
   const fireBaseDatabase = firebase.database();
   var aboutMePicsIndex = 0;
   var LiriNodePicsIndex = 0;
+  var AmazonIndex = 0;
   var aboutMePicsArray;
   var LiriPicsArray;
+  var AmazonArray;
   aboutMePicsCarousel();
   liriNodePicsCarousel();
+  AmazonPicsCarousel();
 
   function aboutMePicsCarousel() {
     var i;
@@ -91,4 +109,54 @@ $(document).ready(function () {
     LiriPicsArray[LiriNodePicsIndex - 1].style.display = "block";
     setTimeout(liriNodePicsCarousel, 6000);
   }
+
+  function AmazonPicsCarousel() {
+    var r;
+    AmazonArray = $(".Amazon-in-node");
+
+    for (r = 0; r < AmazonArray.length; r++) {
+      AmazonArray[r].style.display = "none";
+    }
+
+    AmazonIndex++;
+    if (AmazonArray > AmazonArray.length) {
+      AmazonArray = 1
+    }
+    AmazonArray[AmazonIndex - 1].style.display = "block";
+    setTimeout(AmazonPicsCarousel, 6000);
+  }
+
+  $(document).on("click", "#theme1", function(){
+    $('body').css("background-color", "rgb(255, 255, 255)");
+    $('body').css("color", "rgb(110, 122, 147)");
+    $("#titleScreen").css("background-image", 'url("images/whiteTheme/background2.gif")');
+    $("#titleScreen").attr("id","titleScreen");
+    $('#whatIveWorkedOn').css("border-top", "rgb(110, 122, 147)");
+    $('#whatIveWorkedOn').css("border-bottom", "rgb(110, 122, 147)");
+    $('#whatIveWorkedOn').css("background-color", "rgb(255, 255, 255)");
+    $('.projects').css("background-color", "rgb(255, 255, 255)");
+    $('#aboutmeInfo').css("background-color", "rgb(213, 213, 213)");
+    $(".youTheReader").css("color", "rgb(252, 114, 51)");
+    $("#resume").css("background-color", "rgb(150, 132, 141)");
+    $("#email").css("background-color", "rgb(110, 122, 147)");
+    $("#github").css("background-color", "rgb(213, 213, 213)");
+    $("#linkden").css("background-color", "rgb(255, 255, 255)");
+  });
+
+  $(document).on("click", "#theme2", function(){
+    $('body').css("background-color", "");
+    $('body').css("color", "");
+    $("#titleScreen").css("background-image", 'url("images/title-background3")');
+    $('#whatIveWorkedOn').css("background-color", "");
+    $('#whatIveWorkedOn').css("border-top", "");
+    $('#whatIveWorkedOn').css("border-bottom", "");
+    $('.projects').css("background-color", "");
+    $('#aboutmeInfo').css("background-color", "");
+    $(".youTheReader").css("color", "");
+    $("#resume").css("background-color", "");
+    $("#email").css("background-color", "");
+    $("#github").css("background-color", "");
+    $("#linkden").css("background-color", "");
+  });
+
 });
